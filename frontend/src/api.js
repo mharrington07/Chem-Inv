8acf2333543cbb4753293bed68c3ac7b2b6b80d6
mharrington07 <95518461+mharrington07@users.noreply.github.com>
2024-08-11 14:19:05 -0400

    updating frontend code
    making sure MSDS are updating within the front end

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.NODE_ENV === 'production'
-    ? 'http://YOURURL'  // Replace this with your actual production URL
-    : 'http://LOCALURL',  // Replace with your local network IP and backend port
+    ? 'http://5.161.233.167:5000'  // Replace this with your actual production URL
+    : 'http://localhost:5000',  // Replace with your local network IP and backend port
 });
 
 export default instance;
